1f2a33600ea3b3b9e28f4d0ed91726e6bebd6599
Pascal <pascalyuen@gmail.com>
2022-09-27 22:19:36 +0800

    Let user enter coordinates and update the gameboard

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,17 +1,27 @@
 # Create an empty gameboard
-gameboard = Array.new(3) {Array.new([" ", " ", " "])}
+$gameboard = Array.new(3) {Array.new([" ", " ", " "])}
 
-class Players
+def game
+  puts "Player 1's turn. Enter your x coodinates:"
+  x = gets.to_i
+  puts "Player 1's turn. Enter your y coodinates:"
+  y = gets.to_i
+  play_turn(x, y)
+  print_gameboard
 end
 
-class Player1
-end
-
-class Player2
+def play_turn(x, y)
+  if $gameboard[x][y] == " "
+    $gameboard[x][y] = "X"
+  end
 end
 
 # Print out the gameboard
-gameboard.each_with_index do |row, index|
-  puts "#{row}"
-  puts "\n" unless index == gameboard.length - 1
+def print_gameboard
+  $gameboard.each_with_index do |row, index|
+    puts "#{row}"
+    puts "\n" unless index == $gameboard.length - 1
+  end
 end
+
+game
